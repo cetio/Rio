8c7428e1d81cd03b78539b8c3e22accae840061b
cetio <qubbinyt@gmail.com>
2023-08-31 01:02:19 -0400

    *

--- Emit/Register.cs
+++ Emit/Register.cs
@@ -2,23 +2,55 @@
 {
     public enum Register
     {
-        /// <summary>
-        /// Execution pointer register.
-        /// </summary>
-        EAX,
-        /// <summary>
-        /// Contextual flags register.
-        /// </summary>
-        EBX,
-        /// <summary>
-        /// Comparison result register.
-        /// </summary>
-        ECX,
+        // 1-byte (low) registers
+        AL,
+        BL,
+        CL,
+        DL,
+        R8B,
+        R9B,
+        R10B,
+        R11B,
+        R12B,
+        R13B,
+        R14B,
+        R15B,
 
-        // All registers store their personal flags in the first 4 bytes.
-        // These registers will have the parameters for the function pushed to them.
-        // 64-byte registers
-        R7,
+        // 2-byte (low) registers
+        AX,
+        BX,
+        CX,
+        DX,
+        R8W,
+        R9W,
+        R10W,
+        R11W,
+        R12W,
+        R13W,
+        R14W,
+        R15W,
+
+        // 4-byte (low) registers
+        EAX,
+        EBX,
+        ECX,
+        EDX,
+        R8D,
+        R9D,
+        R10D,
+        R11D,
+        R12D,
+        R13D,
+        R14D,
+        R15D,
+
+        // All registers have a byte at index 0 for flags
+        // Parameters are pushed to registers 2-n
+        // 8-byte registers
+        RAX,
+        RBX,
+        RCX,
+        RDX,
         R8,
         R9,
         R10,
@@ -27,13 +59,8 @@
         R13,
         R14,
         R15,
-        // Extended set
-        RAX,
-        RBX,
-        RCX,
-        RDX,
 
-        // 128-byte registers
+        // 16-byte registers
         XMM0,
         XMM1,
         XMM2,
@@ -44,9 +71,8 @@
         XMM7,
         XMM8,
         XMM9,
-        XMM10,
 
-        // 256-byte registers
+        // 32-byte registers
         YMM0,
         YMM1,
         YMM2,
@@ -57,6 +83,17 @@
         YMM7,
         YMM8,
         YMM9,
-        YMM10
+
+        // 80-byte registers
+        ZMM0,
+        ZMM1,
+        ZMM2,
+        ZMM3,
+        ZMM4,
+        ZMM5,
+        ZMM6,
+        ZMM7,
+        ZMM8,
+        ZMM9,
     }
 }
